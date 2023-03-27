f4452176e721b0c9f686d3cb420645b9b0c89637
Lena Schneider <lena.schneider407@gmail.com>
2023-03-27 20:31:58 +0200

    creating async-await functions to print data from friends.json to the browser

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
 
+// Testing
 app.get('/test', (req, res) => {
   res.send("Banana");
 })
 
+
 app.get('/', (req, res) => {
-  res.send("Hello World");
+  res.send("<h1>Hello World</h1>");
 })
+
+
+// Printing out the data from friends.json
+
+const { readFile } = require('fs').promises
+const gettingFriends = async() => {
+  try {
+    const friends = await readFile('./friends.json', 'utf-8');
+    // console.log(friends);
+    return friends
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get('/friends', async (req, res) => {
+  try {
+    const friends = await gettingFriends();
+    console.log(friends);
+    res.send(friends)
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
 
 module.exports = app
