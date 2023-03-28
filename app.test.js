a9de671df28e6fcc45765cb3ab612d5e7806068f
Lena Schneider <lena.schneider407@gmail.com>
2023-03-28 20:50:17 +0200

    test 1 gettingJsonData

--- app.test.js
+++ app.test.js
@@ -6,14 +6,21 @@ const friends = require('./app')
 // gettingJsonData()
 test('Fetches data from ./friends.json', async () => {
   const jsonData = await gettingJsonData()
-  console.log(jsonData);
+  const realData = [
+    { "id": 1, "name": "nandha", "importance": "A", "lastContacted": 2 },
+    { "id": 2, "name": "marco", "importance": "B", "lastContacted": 1 },
+    { "id": 3, "name": "caro", "importance": "C", "lastContacted": 3 }
+  ]
+  // expect(gettingJsonData()).toEqual(realData)
+  expect(jsonData).toEqual(realData)
 })
 
 // friends()
-test('Fetches data from ./friends.json', async () => {
-  const jsonData = await gettingJsonData()
-  console.log(jsonData);
-})
+// test('Converts json to js object', async () => {
+//   const data = await friends();
+//   console.log(data);
+//   // actual test
+// })
 
 // const gettingJsonData = async() => {
 //   const jsonData = await readFile('./friends.json', 'utf-8');
