34c2900818e196cf419428878605f35b2b42f40e
Lena Schneider <lena.schneider407@gmail.com>
2023-03-29 18:56:01 +0200

    server.js not working anymore for some reason; moved code to app.js

--- app.js
+++ app.js
@@ -105,6 +105,10 @@ app.get('/api/v1/query', async (req, res) => {
 
 
 
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+})
+
 
 module.exports = app
 module.exports = gettingJsonData
