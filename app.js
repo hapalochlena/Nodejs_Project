752d7558962b6c0aab52bcab47a6cf4194087ad2
Lena Schneider <lena.schneider407@gmail.com>
2023-04-06 23:06:32 +0200

    eslint set up; format on save enabled

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 
 
 // * IMPORT THE ROUTES from Router
-const homeRoute = require('./routes/homepage-router')
-const friendsRoutes = require('./routes/friends-router')
+const homeRoute = require('./routes/homepage-router');
+const friendsRoutes = require('./routes/friends-router');
 // ***
 
 
@@ -17,21 +17,21 @@ const friendsRoutes = require('./routes/friends-router')
 // * MIDDLEWARE - requests pass through here before they go to controller / functions anywhere else can use the built-in middleware from here
 // app.use(express.static('./methods-public'))
 // app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.json());
 // app.use(logger) // * ––> notice we don't need route as argument here, because this is what every request passes through, so we would have to put it as argument into every single route; insteaad we put it into app.use here
 // app.use(authorize) // * ––> notice we don't need route as argument here, because this is what every request passes through, so we would have to put it as argument into every single route; insteaad we put it into app.use here
 // ***
 
 
 // * ROUTES
-app.use('/', homeRoute) // ? might not need this if you have index.html as homepage in 'public' and use app.use(express.static('./public')) => that will automatically be the homepage ?
-app.use('/friends', friendsRoutes)
+app.use('/', homeRoute); // ? might not need this if you have index.html as homepage in 'public' and use app.use(express.static('./public')) => that will automatically be the homepage ?
+app.use('/friends', friendsRoutes);
 // ***
 
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000...");
-})
+	console.log('Listening on port 3000...');
+});
 
 
 
@@ -45,6 +45,6 @@ app.listen(3000, () => {
 // app.use(express.static('./public'));
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, './public/index.html'))
-      // * OPTION 1) ADDING TO STATIC ASSETS
-      // * OPTION 2) SSR
+// *      OPTION 1) ADDING TO STATIC ASSETS
+// *      OPTION 2) SSR
 // })
--- routes/friends-router.js
+++ routes/friends-router.js
@@ -2,47 +2,47 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  gettingJsonData,
-  gettingFriends,
-  selectingFriend,
-  updatingFriend,
-  deletingFriend,
-  queryingFriends
-} = require('../controllers/friends-controller')
+	gettingJsonData,
+	gettingFriends,
+	selectingFriend,
+	updatingFriend,
+	deletingFriend,
+	queryingFriends
+} = require('../controllers/friends-controller');
 
 // * GET
 
 router.get('/', [gettingJsonData, gettingFriends], (req, res) => {
-  // const output = JSON.stringify(req.friendsData)
-  // res.status(200).json({success: true, data: output})
-  res.status(200).send(req.friendsData)
+	// const output = JSON.stringify(req.friendsData)
+	// res.status(200).json({success: true, data: output})
+	res.status(200).send(req.friendsData);
 });
 
 router.get('/:id', [gettingJsonData, gettingFriends, selectingFriend]);
 
 // ? 'query' needs to have 'api' before in the route; '/friends/query' doesn't work
-router.get('/api/query', [gettingJsonData, gettingFriends, queryingFriends])
+router.get('/api/query', [gettingJsonData, gettingFriends, queryingFriends]);
 
 // ***
 
 // * POST, PUT, DELETE
 
 router.post('/postman', (req, res) => {
-  // const { username } = req.body
-  console.log(req.body);
-  // res.status(201) = successful post request
-  // res.status(400) = bad request
-        // .json({ success: false, msg: '...' })
-})
+	// const { username } = req.body
+	console.log(req.body);
+	// res.status(201) = successful post request
+	// res.status(400) = bad request
+	// .json({ success: false, msg: '...' })
+});
 
-router.put('/:id', [gettingJsonData, gettingFriends, updatingFriend])
+router.put('/:id', [gettingJsonData, gettingFriends, updatingFriend]);
 
-router.delete('/:id', [gettingJsonData, gettingFriends, deletingFriend])
+router.delete('/:id', [gettingJsonData, gettingFriends, deletingFriend]);
 
 // ***
 
 
-module.exports = router
+module.exports = router;
 
 
 
