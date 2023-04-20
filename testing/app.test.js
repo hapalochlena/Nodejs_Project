e56c20d3eb4b2e04b97b4cae67adb1c0ba92100e
Lena Schneider <lena.schneider407@gmail.com>
2023-04-20 10:05:14 +0200

    test gh actions

--- testing/app.test.js
+++ testing/app.test.js
@@ -1,9 +1,9 @@
-const { default: test } = require('node:test')
-const app = require('../app') // oder const { app } = require('./app') ??????
-const gettingJsonData = require('../app') // ? why not './middleware/middleware' ???
-const gettingFriends = require('../app') // ? why not './middleware/middleware' ???
-const selectingFriend = require('../app') // ? why not './middleware/middleware' ???
-const queryingFriends = require('../app') // ? why not './middleware/middleware' ???
+// const { default: test } = require('node:test');
+const app = require('../app'); // oder const { app } = require('./app') ??????
+const gettingJsonData = require('../app'); // ? why not './middleware/middleware' ???
+const gettingFriends = require('../app'); // ? why not './middleware/middleware' ???
+const selectingFriend = require('../app'); // ? why not './middleware/middleware' ???
+const queryingFriends = require('../app'); // ? why not './middleware/middleware' ???
 
 // * ROUTES
 
@@ -18,28 +18,28 @@ const queryingFriends = require('../app') // ? why not './middleware/middleware'
 // ? should respond with a status code of 400
 
 // app.get('/')
-test('/', () => {
-  expect().
-})
+// test('/', () => {
+//   expect().
+// })
 
 // app.get('/friends', (req, res) => {
 //   res.status(200).send(req.friendsData)
 // });
-test('/friends', () => {
-  expect().
-})
+// test('/friends', () => {
+//   expect().
+// })
 
 // app.get('/friends/:id', selectingFriend, (req, res) => {
 // });
-test('/friends/:id', () => {
-  expect().
-})
+// test('/friends/:id', () => {
+//   expect().
+// })
 
 // app.get('/friends/api/query', queryingFriends, (req, res) => {
 // })
-test('/friends/api/query', () => {
-  expect().
-})
+// test('/friends/api/query', () => {
+//   expect().
+// })
 
 // app.listen(3000, () => {
 //   console.log("Listening on port 3000...");
@@ -75,20 +75,20 @@ test('/friends/api/query', () => {
 // ---------------------------------------------------
 
 // gettingFriends()
-test('Converts json to js object', () => {
-  // ? Do I need those here ???
-  const req = {}
-  const res = {}
-  const next = jest.fn()
+// test('Converts json to js object', () => {
+// 	// ? Do I need those here ???
+// 	// const req = {};
+// 	// const res = {};
+// 	// const next = jest.fn();
 
-  const objectData = gettingFriends(req, res, next);
-  const expectedObject = {}
+// 	const objectData = gettingFriends(req, res, next);
+// 	const expectedObject = {};
 
-  console.log(typeof objectData); // object
-  console.log(typeof expectedObject); // object
+// 	console.log(typeof objectData); // object
+// 	console.log(typeof expectedObject); // object
 
-  expect(typeof objectData).toStrictEqual(typeof expectedObject)
-})
+// 	expect(typeof objectData).toStrictEqual(typeof expectedObject);
+// });
 
 // ---------------------------------------------------
 
@@ -101,16 +101,16 @@ test('Converts json to js object', () => {
 //   expect(selectedFriend.id).toEqual(trueId)
 // })
 
-test('Returns Marco for id = 2, and 404 if there is no friend with this id', () => {
-  const req = {}
-  const res = {}
-  const next = jest.fn()
+// test('Returns Marco for id = 2, and 404 if there is no friend with this id', () => {
+// 	// const req = {};
+// 	// const res = {};
+// 	// const next = jest.fn();
 
-  const selectedFriend = selectingFriend(req, res, next)
-  // ! selectedFriend = {}
-  console.log(JSON.stringify(selectedFriend) + "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
-  expect(selectedFriend.id).toBe(2)
-})
+// 	const selectedFriend = selectingFriend(req, res, next);
+// 	// ! selectedFriend = {}
+// 	console.log(JSON.stringify(selectedFriend) + '<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+// 	expect(selectedFriend.id).toBe(2);
+// });
 
 // test('Returns Marco for id = 2, and 404 if there is no friend with this id', () => {
 //   const req = { params: {id : '2'} }
