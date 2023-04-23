109e70a6f25904d7baabbbfb696d721e40ef135d
Lena Schneider <lena.schneider407@gmail.com>
2023-04-23 04:07:44 +0200

    babel installed; first new test done

--- tests/friends-logic.test.js
+++ tests/friends-logic.test.js
@@ -1,3 +1,5 @@
+const express = require('express');
+
 const {
 	fetchingFriends,
 	selectingFriend,
@@ -6,13 +8,14 @@ const {
 } = require('../business-logic/friends-logic');
 
 describe('fetchingFriends', () => {
-	test('fetches data from json file and converts it into js object', () => {
+	test('fetches data from json file and converts it into js object', async () => {
 		const friendsData = [
 			{ id: 1, name: 'nandha', importance: 'A', lastContacted: 2 },
 			{ id: 2, name: 'marco', importance: 'B', lastContacted: 1 },
 			{ id: 3, name: 'caro', importance: 'C', lastContacted: 3 }
 		];
 
-		expect(fetchingFriends()).toStrictEqual(friendsData);
+		const result = await fetchingFriends();
+		expect(result).toStrictEqual(friendsData);
 	});
 });
