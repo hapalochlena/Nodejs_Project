98813539575d955a14e44390d6f3a35c65cd9383
Lena Schneider <lena.schneider407@gmail.com>
2023-05-29 17:02:11 +0200

    tried api function with serverless-http removed, now it can't find module 'express'

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
 // const serverless = require('serverless-http');
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
 
-// app.get('/', (req, res, next) => {
-//   return res.status(200).json({
-//     message: 'Hello from root!',
-//   });
-// });
+app.get('/', (req, res, next) => {
+  return res.status(200).json({
+    message: 'Hello from root!',
+  });
+});
 
-// app.get('/path', (req, res, next) => {
-//   return res.status(200).json({
-//     message: 'Hello from path!',
-//   });
-// });
+app.get('/path', (req, res, next) => {
+  return res.status(200).json({
+    message: 'Hello from path!',
+  });
+});
 
-// app.use((req, res, next) => {
-//   return res.status(404).json({
-//     error: 'Not Found',
-//   });
-// });
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: 'Not Found',
+  });
+});
 
-// module.exports.handler = serverless(app);
-// // * handler = lambda handler
+module.exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Your function executed successfully!',
+        input: event
+      },
+      null,
+      2
+    )
+  };
+};
+
+// * handler = lambda handler
 
 module.exports.hello = async (event) => {
   return {
