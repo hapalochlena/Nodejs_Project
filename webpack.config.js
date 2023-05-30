ae628bc16b8d9f24e3f3545c348b90f58c6179c7
Lena Schneider <lena.schneider407@gmail.com>
2023-05-30 19:29:48 +0200

    serverless, webpack and serverless-webpack reinstalled; dependency errors when trying to deploy

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  target: 'node', // what are we trying to compile down
+  mode: 'production', // => minifies it as much as possible
+  // mode: 'none' // => we are not in a production environment, so don't need the extra processing to minify the code
+};
