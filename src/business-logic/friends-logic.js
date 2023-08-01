6612d6f55fb550309b88730fa14250bf2c3c1b64
Lena Schneider <lena.schneider407@gmail.com>
2023-08-01 12:09:56 +0200

    changed db configuration; added necessary env variables

--- src/business-logic/friends-logic.js
+++ src/business-logic/friends-logic.js
@@ -1,34 +1,17 @@
-// * LOCAL db Connection
+// * PROD stage ––> RDS Connection
 
-// const knex = require('knex');
+//* You need to update your knex.js configuration to connect to the AWS RDS PostgreSQL database.
 
-// const db = knex({
-// 	client: 'pg', // pg = posgres
-// 	connection: {   // tell it where the database lives
-// 		host : '127.0.0.1', // 127.0.0.1 = localhost => later different when hosted on a separate platform
-// 		port : 5432,  // port for Postgres
-// 		user : 'Lena',  // name as appears in the 'Owner' column when you do \d to see db tables
-// 		password : '',
-// 		database : 'friend-reminder'
-// 	}
-// });
-
-///////////////
+require('dotenv').config();
 
 const knex = require('knex');
 const db = knex({
   client: 'pg',
-  connection: {   // tell it where the database lives
-    // ! host : '127.0.0.1', // 127.0.0.1 = localhost => later different when hosted on a separate platform
-    port : 5432,  // port for Postgres
-    user : 'Lena',  // name as appears in the 'Owner' column when you do \d to see db tables
-    // ! password : '',
-    database : 'friend-reminder'
-
-    // host: process.env.DB_HOST,
-    // user: process.env.DB_USER,
-    // password: process.env.DB_PASS,
-    // database: process.env.DB_NAME
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   }
   // migrations: {
   //   tableName: 'knex_migrations',
@@ -39,22 +22,29 @@ const db = knex({
   // }
 });
 
-// ! dotenv
+//* The rest of your CRUD operations should remain the same as they were for your local PostgreSQL database.
+//* Knex.js abstracts away the specifics of the database, so you don't need to change your queries.
 
 const fetchingFriends = () => {
 	return db.select('*').from('friends')
-		// .then(data => data)
-    .then((data) => {
-      console.log('received data: ', data);
-      db.client.destroy(); // !
-      return data;
-    })
-		// .catch(error => console.log(error));
-    .catch((error) => {
-      console.log('error occurred: ', error);
-      db.client.destroy(); // !
-    });
+		.then(data => data)
+		.catch(error => console.log(error));
 };
+
+// const fetchingFriends = () => {
+// 	return db.select('*').from('friends')
+// 		// .then(data => data)
+//     .then((data) => {
+//       // console.log('received data: ', data);
+//       db.client.destroy(); // !
+//       return data;
+//     })
+// 		// .catch(error => console.log(error));
+//     .catch((error) => {
+//       // console.log('error occurred: ', error);
+//       db.client.destroy(); // !
+//     });
+// };
 
 // exports.handler = (event, context, callback) => {
 //   console.log('event received: ', event);
@@ -80,46 +70,6 @@ const fetchingFriends = () => {
 //       return callback(err);
 //     });
 // };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -191,6 +141,128 @@ const deletingFriendLogic = (id) => {
     .then(data => data) // without 'returning' returns 1 -> promise resolves to the number of rows deleted = 1
     .catch(error => console.log(error));
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////
+
+
+// * DEV stage ––> LOCAL db Connection
+
+// const knex = require('knex');
+
+// const db = knex({
+// 	client: 'pg', // pg = posgres
+// 	connection: {   // tell it where the database lives
+// 		host : '127.0.0.1', // 127.0.0.1 = localhost => later different when hosted on a separate platform
+// 		port : 5432,  // port for Postgres
+// 		user : 'Lena',  // name as appears in the 'Owner' column when you do \d to see db tables
+// 		password : '',
+// 		database : 'friend-reminder'
+// 	}
+// });
+
+// const fetchingFriends = () => {
+// 	return db.select('*').from('friends')
+// 		.then(data => data)
+// 		.catch(error => console.log(error));
+// };
+
+// const selectingFriend = (id) => {
+//   return db('friends').where('id', id)
+//     .then(data => data)
+//     .catch(error => console.log(error));
+// };
+
+// const creatingFriendLogic = ({name, importance = null, lastContacted = null}) => {
+// 	const currentDate = new Date();
+// 	const isoDate = currentDate.toISOString();
+// 	console.log(name); // Nandha
+// 	console.log(importance); // A
+// 	console.log(lastContacted); // null
+
+// 	db('friends').insert({
+// 		name: name,
+// 		importance: importance,
+// 		last_contacted: isoDate, // ! should be lastContacted
+// 		user_id: 1, // ! userId
+// 		added: isoDate
+// 	}).then(console.log).catch(error => console.log(error));
+// };
+
+// const updatingFriendLogic = async (id, properties) => {
+//   // console.log(properties); // { name: 'Elly', importance: 'B' }
+//   for (const [key, value] of Object.entries(properties)) {
+//     if (key === 'name') {
+//       // console.log(name);
+//       db('friends')
+//         .where('id', '=', id)
+//         .update({
+//           name: value
+//         })
+//         .returning('friends')
+//         .then(data => data)
+//         .catch(error => console.log(error));
+//     } else if (key === 'importance') {
+//       db('friends')
+//         .where('id', '=', id)
+//         .update({
+//           importance: value
+//         })
+//         .returning('friends')
+//         .then(data => data)
+//         .catch(error => console.log(error));
+//     } else if (key === 'lastContacted') {
+//       return db('friends')
+//         .where('id', '=', id)
+//         .update({
+//           last_contacted: value
+//         })
+//         .returning('friends')
+//         .then(data => data)
+//         .catch(error => console.log(error));
+//     }
+//   }
+// };
+
+// const deletingFriendLogic = (id) => {
+//   return db('friends')
+//     .where('id', id)
+//     .returning('*') // now the promise resolves with an array of the deleted rows, which contains the data of the deleted row(s)
+//     .del()
+//     .then(data => data) // without 'returning' returns 1 -> promise resolves to the number of rows deleted = 1
+//     .catch(error => console.log(error));
+// };
 
 
 
